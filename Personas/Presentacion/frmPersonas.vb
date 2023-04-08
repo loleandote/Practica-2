ff8d3bd678a9b83568f78845766fe1ac45db9255
loleandote <48246750+loleandote@users.noreply.github.com>
2023-04-08 12:22:15 +0200

    sin Me.

--- Personas/Presentacion/frmPersonas.vb
+++ Personas/Presentacion/frmPersonas.vb
@@ -1,56 +1,42 @@
 ﻿Public Class frmPersonas
     Private p As Persona
-    'Private lstPersonas As ListBox
-    'Private btnAbrirBD As Button
-    'Private btnAnadir As Button
-    'Private btnActualizar, btnEliminar, btnLimpiar As Button
-    'Private txtID, txtNombre As TextBox
 
 
-
-    Public Sub New()
-
-        ' Esta llamada es exigida por el diseñador.
-        InitializeComponent()
-
-        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
-
-    End Sub
     Private Sub BtnAbrirBD_Click(sender As Object, e As EventArgs) Handles btnAbrirBD.Click
         Dim pAux As Persona
-        Me.p = New Persona
+        p = New Persona
         Try
-            Me.p.LeerTodasPersonas()
+            p.LeerTodasPersonas()
         Catch ex As Exception
             MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
             Exit Sub
         End Try
-        For Each pAux In Me.p.PerDAO.Personas
-            Me.lstPersonas.Items.Add(pAux.IDPersona)
+        For Each pAux In p.PerDAO.Personas
+            lstPersonas.Items.Add(pAux.IDPersona)
         Next
         btnAbrirBD.Enabled = False
         btnAnadir.Enabled = True
     End Sub
 
     Private Sub LstPersonas_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstPersonas.SelectedIndexChanged
-        Me.btnActualizar.Enabled = True
-        Me.btnEliminar.Enabled = True
-        Me.txtID.Enabled = False
-        If Not Me.lstPersonas.SelectedItem Is Nothing Then
-            Me.p = New Persona(Me.lstPersonas.SelectedItem.ToString)
+        btnActualizar.Enabled = True
+        btnEliminar.Enabled = True
+        txtID.Enabled = False
+        If Not lstPersonas.SelectedItem Is Nothing Then
+            p = New Persona(lstPersonas.SelectedItem.ToString)
             Try
-                Me.p.LeerPersona()
+                p.LeerPersona()
             Catch ex As Exception
                 MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
                 Exit Sub
             End Try
-            Me.txtID.Text = Me.p.IDPersona.ToString
-            Me.txtNombre.Text = Me.p.Nombre.ToString
+            txtID.Text = .p.IDPersona.ToString
+            txtNombre.Text = p.Nombre.ToString
         End If
     End Sub
 
     Private Sub BtnAnadir_Click(sender As Object, e As EventArgs) Handles btnAnadir.Click
-        If Me.txtID.Text <> String.Empty And Me.txtNombre.Text <> String.Empty Then
+        If txtID.Text <> String.Empty And txtNombre.Text <> String.Empty Then
             p = New Persona
             p.IDPersona = txtID.Text
             p.Nombre = txtNombre.Text
@@ -64,7 +50,7 @@
                 MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
                 Exit Sub
             End Try
-            Me.lstPersonas.Items.Add(p.IDPersona)
+            lstPersonas.Items.Add(p.IDPersona)
         End If
     End Sub
 
@@ -89,10 +75,10 @@
     End Sub
 
     Private Sub BtnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
-        If Not Me.p Is Nothing Then
-            If MessageBox.Show("¿Estás seguro que quieres borrar " & Me.p.IDPersona & "?", "Por favor, confirma", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+        If Not p Is Nothing Then
+            If MessageBox.Show("¿Estás seguro que quieres borrar " & p.IDPersona & "?", "Por favor, confirma", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
                 Try
-                    If Me.p.BorrarPersona() <> 1 Then
+                    If p.BorrarPersona() <> 1 Then
                         MessageBox.Show("DELETE return <> 1", String.Empty, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
                         Exit Sub
                     End If
@@ -100,19 +86,19 @@
                     MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
                     Exit Sub
                 End Try
-                Me.lstPersonas.Items.Remove(p.IDPersona)
+                lstPersonas.Items.Remove(p.IDPersona)
             End If
-            Me.btnLimpiar.PerformClick()
+            btnLimpiar.PerformClick()
         End If
 
     End Sub
 
     Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
-        Me.txtID.Text = String.Empty
-        Me.txtNombre.Text = String.Empty
-        Me.btnActualizar.Enabled = False
-        Me.btnEliminar.Enabled = False
-        Me.txtID.Enabled = True
+        txtID.Text = String.Empty
+        txtNombre.Text = String.Empty
+        btnActualizar.Enabled = False
+        btnEliminar.Enabled = False
+        txtID.Enabled = True
     End Sub
 
 
